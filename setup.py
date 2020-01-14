c5e0d72d1ae009fbc8a724e701597166b8bf1cd0
Chang Hsin Lee <lee.changhsin@gmail.com>
2020-01-14 08:05:45 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mock_examples',
+      version='0.1',
+      description='Examples for using pytest mock',
+      author='Chang Hsin Lee',
+      license='MIT',
+      packages=['mock_examples'],
+      python_requires='>=3.5',
+      zip_safe=False)
