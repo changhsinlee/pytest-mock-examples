2500923057514a4fa447ddc64e69c46da6da533e
Zsolt Háló <net.zsolt.net@gmail.com>
2020-08-21 12:04:21 +0200

    dependency fix in setup.py (#1)

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='mock_examples',
       description='Examples for using pytest mock',
       author='Chang Hsin Lee',
       license='MIT',
-      packages=['mock_examples', 'pytest', 'pytest-mock'],
+      packages=['mock_examples'],
+      install_requires=['pytest', 'pytest-mock'],
       python_requires='>=3.5',
       zip_safe=False)
